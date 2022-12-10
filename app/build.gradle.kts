0f60b976fb7dd3d021dda53843e762bfb3e20863
floren <florendg@gmail.com>
2022-12-10 11:00:53 +0100

    upgrade to java 19 add jpackage step Refactor VarHeader parser

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,16 +10,49 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
 }
 
+val appMainClassName = "dev.vultureweb.iracing.telemetry.app.Main"
+val appModuleName = "dev.vultureweb.iracing.telemetry.app"
+val appName = "iracing-parser"
+
+val compiler = javaToolchains.compilerFor {
+    languageVersion.set(JavaLanguageVersion.of(JavaVersion.VERSION_19.majorVersion))
+}
+
 java {
     modularity.inferModulePath.set(true);
+    toolchain {
+        vendor.set(JvmVendorSpec.ADOPTIUM)
+        languageVersion.set(JavaLanguageVersion.of(19))
+    }
 }
+
 application {
-    mainClass.set("dev.vultureweb.iracing.telemetry.app.Main")
-    mainModule.set("dev.vultureweb.iracing.telemetry.app")
+    mainClass.set(appMainClassName)
+    mainModule.set(appModuleName)
 }
 
 tasks {
     test {
         useJUnitPlatform()
     }
+
+    task<Copy>("copyDependencies") {
+        from(configurations.runtimeClasspath)
+        into("$buildDir/modules")
+    }
+
+    task<Exec>("package") {
+        dependsOn(listOf("build", "copyDependencies"))
+        val jdkHome = compiler.get().metadata.installationPath.asFile.absolutePath
+        commandLine("${jdkHome}/bin/jpackage")
+        args(listOf(
+            "-n", "${appName}",
+            "-p", "$buildDir/modules"+File.pathSeparator+"$buildDir/libs",
+            "-d", "$buildDir/installer",
+            "-m", "${appModuleName}/${appMainClassName}"))
+    }
 }
+
+
+
+
--- app/src/main/java/dev/vultureweb/iracing/telemetry/app/model/VarInfo.java
+++ app/src/main/java/dev/vultureweb/iracing/telemetry/app/model/VarInfo.java
@@ -1,4 +1,19 @@
 package dev.vultureweb.iracing.telemetry.app.model;
 
+import java.nio.ByteBuffer;
+
 public record VarInfo(int type, int offset, int count, boolean countAsTime) {
+
+   public static int VAR_INFO_SIZE = 16;
+
+   public static VarInfo fromByteBuffer(ByteBuffer buffer) {
+      if(VAR_INFO_SIZE != buffer.remaining()) {
+         throw new IllegalArgumentException("buffer size for VarInfo should be 16 bytes");
+      }
+      int type = buffer.getInt();
+      int offset = buffer.getInt();
+      int count = buffer.getInt();
+      int countAsTime = buffer.getInt();
+      return new VarInfo(type, offset, count, countAsTime == 1);
+   }
 }
